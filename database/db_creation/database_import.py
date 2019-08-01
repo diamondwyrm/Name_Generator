27233f3580547d37d8b899ce042e2cb9fe9b7aec
diamondwyrm <kent.j.hazen@biola.edu>
2019-08-01 16:53:32 -0700

    work done towards organizing the dataset into a usable format

--- database/db_creation/database_import.py
+++ database/db_creation/database_import.py
@@ -6,16 +6,24 @@ connection = sqlite3.connect('../name_origins.sqlite')
 
 # read in category.csv
 category_df = pd.read_csv('../name-origin-csvs/category.csv')
-category_df.to_sql('category', connection)
+category_df = category_df.set_index("id")
+category_df.to_sql('category', connection, if_exists="replace", index=True, index_label="id")
 
 # read in category_to_name.csv
 category_to_name_df = pd.read_csv('../name-origin-csvs/category_to_name.csv')
-category_to_name_df.to_sql('category_to_name', connection)
+category_to_name_df = category_to_name_df.set_index("id")
+category_to_name_df.to_sql('category_to_name', connection, if_exists="replace", index=True, index_label="id")
 
 # read in name.csv
 name_df = pd.read_csv('../name-origin-csvs/name.csv')
-name_df.to_sql('name', connection)
+name_df = name_df.set_index("id")
+name_df.to_sql('name', connection, if_exists="replace", index=True, index_label="id")
 
 # read in origin.csv
 origin_df = pd.read_csv('../name-origin-csvs/origin.csv')
-origin_df.to_sql('origin', connection)
+origin_df = origin_df.set_index("id")
+origin_df.to_sql('origin', connection, if_exists="replace", index=True, index_label="id")
+
+
+# close connection
+connection.close()
